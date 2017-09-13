72a08455211f83e56a293041d34956b182a69413
dnovaes <novaesdigo@gmail.com>
2017-09-12 21:29:02 -0300

    changed console.log text on after window focused.

--- extFunctions/focus.js
+++ extFunctions/focus.js
@@ -2,6 +2,5 @@
 const addon = require("./build/Release/addon");
 
 addon.focusWindow(function(res){
-  console.log(res);
-  console.log("Screen Resolution is now set.");
+  console.log("Screen PXG focused.");
 });
