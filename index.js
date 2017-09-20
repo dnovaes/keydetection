0099c121a5f7d1bd5eec9d4135b6bc4597a83cfe
dnovaes <novaesdigo@gmail.com>
2017-09-19 23:21:59 -0300

    initial screen now is login.html, user needs to log in before enter in the app

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ const url = require('url')
 let win
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 450, height: 300, frame: false, resizable: false, backgroundThrottling: false })
+  // Create the browser window. backgroundThrottling: false
+  win = new BrowserWindow({width: 460, height: 310, frame: false, resizable: false })
   win.setMenu(null);
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'login.html'),
     protocol: 'file:',
     slashes: true
   }))
+  win.show();
 
   // Open the DevTools.
   //win.webContents.openDevTools()
